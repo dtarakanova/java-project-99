0a94ded4f7245dd0633f6bc3e13f63aef79bbddc
dtarakanova <dshargalina@gmail.com>
2024-07-30 08:53:04 +0300

    Rework project structure

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    application
+    id("io.freefair.lombok") version "8.6"
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.AppApplication") }
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
